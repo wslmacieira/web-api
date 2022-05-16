6cb590d28ae49b77f3454f1a65dbd782937e2e75
wslmacieira <wslmacieira@outlook.com>
2022-05-16 13:15:05 -0300

    creating and configuring routes

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,26 @@
 import { parse } from 'node:url'
+import { DEFAULT_HEADER } from './util/util.js'
+
+const allRoutes = {
+  '/heroes:get': (request, response) => {
+    response.write('GET')
+    response.end()
+  },
+  // 404 routes
+  default: (request, response) => {
+    response.write('uuuuups, not found!')
+    response.writeHead(404, DEFAULT_HEADER)
+    response.end()
+  }
+}
 
 function handler(request, response) {
   const { url, method } = request
   const { pathname } = parse(url, true)
   const key = `${pathname}:${method.toLowerCase()}`
+  const chosen = allRoutes[key] || allRoutes.default
 
-  console.log({ key })
-  response.end('Hello World')
+  return chosen(request, response)
 }
 
 export default handler
