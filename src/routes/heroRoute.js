2b86350419bcf1bd8ce92ff35908d9dfa3a52f35
wslmacieira <wslmacieira@outlook.com>
2022-05-17 16:21:07 -0300

    creating entity and update heroRoute

--- src/routes/heroRoute.js
+++ src/routes/heroRoute.js
@@ -1,3 +1,7 @@
+import { once } from 'node:events'
+import Hero from '../entities/hero.js'
+import { DEFAULT_HEADER } from '../util/util.js'
+
 const routes = ({
   heroService
 }) => ({
@@ -6,7 +10,17 @@ const routes = ({
     response.end()
   },
   '/heroes:post': async (request, response) => {
-    response.write('POST')
+    const data = await once(request, 'data')
+    const item = JSON.parse(data)
+    const hero = new Hero(item)
+
+    const id = hero.id
+    response.writeHead(201, DEFAULT_HEADER)
+    response.write(JSON.stringify({
+      id,
+      success: 'User created with success!!',
+
+    }))
     response.end()
   },
 })
